6e08a348509f8942398ebb6f5cd2951acf6bca39
JuanTrop <jortiza2@unicartagena.edu.co>
2020-11-25 16:53:00 -0500

    Agregar archivos de proyecto.

---
+++ InicioUI.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Fase4MaidyYarlediVega
+{
+    partial class InicioUI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblLicencia = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(204, 56);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(431, 31);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "ARBOL BINARIO DE BUSQUEDA";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese la contraseña de acceso";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(19, 47);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(67, 13);
+            this.lblContraseña.TabIndex = 2;
+            this.lblContraseña.Text = "Contraseña: ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtContraseña);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblContraseña);
+            this.panel1.Location = new System.Drawing.Point(227, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(347, 144);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(101, 44);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(137, 20);
+            this.txtContraseña.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(3, 26);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(140, 13);
+            this.lblAutor.TabIndex = 4;
+            this.lblAutor.Text = "Maidy Yarledi Vega Castaño";
+            this.lblAutor.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.datePicker);
+            this.panel2.Controls.Add(this.lblLicencia);
+            this.panel2.Controls.Add(this.lblAutor);
+            this.panel2.Location = new System.Drawing.Point(46, 281);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(712, 67);
+            this.panel2.TabIndex = 5;
+            // 
+            // lblLicencia
+            // 
+            this.lblLicencia.AutoSize = true;
+            this.lblLicencia.Location = new System.Drawing.Point(569, 26);
+            this.lblLicencia.Name = "lblLicencia";
+            this.lblLicencia.Size = new System.Drawing.Size(138, 13);
+            this.lblLicencia.TabIndex = 5;
+            this.lblLicencia.Text = "Licencia Creative Commons";
+            // 
+            // datePicker
+            // 
+            this.datePicker.CausesValidation = false;
+            this.datePicker.Location = new System.Drawing.Point(252, 19);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 20);
+            this.datePicker.TabIndex = 6;
+            // 
+            // InicioUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 360);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "InicioUI";
+            this.Text = "Fase 4 Diseño de arboles binarios";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label lblLicencia;
+    }
+}
+
